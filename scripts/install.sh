e96b06b075cbb747a6cb7756eed91eeb72f6c86c
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2020-05-04 18:14:00 +0100

    add provide amazon-ebs

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,31 +7,29 @@ CWD=$(cd $(dirname $0); pwd)
 ##############################################################
 echo "Configuring system ..."
 
-## encoding
-cat >> /etc/environment << EOF
-LANG=en_US.UTF-8
-LC_ALL=en_US.UTF-8
-EOF
-
 ## timezone
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 
 ## disable firewall and iptables
-systemctl disable firewalld.service
+#sudo systemctl disable firewalld.service
 
 ## disable kdump
-systemctl disable kdump.service
+#sudo systemctl disable kdump.service
 
 ##############################################################
 # Running ovfenv-installer for next reboot
 ##############################################################
-yum install -y https://github.com/subchen/ovfenv-installer/releases/download/v1.0.2/ovfenv-installer-1.0.2-17.x86_64.rpm
+#sudo yum install -y https://github.com/subchen/ovfenv-installer/releases/download/v1.0.2/ovfenv-installer-1.0.2-17.x86_64.rpm
 
-cat >> /etc/rc.d/rc.local << EOF
-ovfenv-installer --run-once --log-file=/var/log/ovfenv-installer.log
-EOF
+#sudo cat >> /etc/rc.d/rc.local << EOF
+#ovfenv-installer --run-once --log-file=/var/log/ovfenv-installer.log
+#EOF
 
-chmod +x /etc/rc.d/rc.local
+#sudo chmod +x /etc/rc.d/rc.local
+
+# Disable selinux
+setenforce 0
+sed -i 's/enforcing/disabled/' /etc/selinux/config
 
 # Performe a update in system
 
